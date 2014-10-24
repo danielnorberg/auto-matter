6d5275b03184d52f2ba3c70bf0bc709ec2752df6
Daniel Norberg <daniel.norberg@gmail.com>
2014-10-24 11:36:53 +0900

    jackson: verify serialization configurability

--- jackson/src/test/java/io/norberg/automatter/jackson/Foo.java
+++ jackson/src/test/java/io/norberg/automatter/jackson/Foo.java
@@ -1,9 +1,15 @@
 package io.norberg.automatter.jackson;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.norberg.automatter.AutoMatter;
 
 @AutoMatter
 public interface Foo {
   int a();
   String b();
+  boolean aCamelCaseField();
+
+  @JsonProperty("foobar")
+  boolean isReallyFoobar();
 }
