4f684124fed3ac9bdd975f522a628158b351c476
Daniel Norberg <daniel.norberg@gmail.com>
2014-11-26 21:50:21 +0900

    test nullable optional

--- test/src/test/java/io/norberg/automatter/GuavaOptionalTest.java
+++ test/src/test/java/io/norberg/automatter/GuavaOptionalTest.java
@@ -7,6 +7,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import javax.annotation.Nullable;
+
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -19,6 +22,8 @@ public class GuavaOptionalTest {
 
     Optional<String> foo();
     Optional<String> bar();
+
+    @Nullable Optional<String> baz();
   }
 
   GuavaOptionalsBuilder builder;
@@ -33,6 +38,7 @@ public class GuavaOptionalTest {
     final GuavaOptionals foobar = builder.build();
     assertThat(foobar.foo(), is(Optional.<String>absent()));
     assertThat(foobar.bar(), is(Optional.<String>absent()));
+    assertThat(foobar.baz(), is(nullValue()));
   }
 
   @Test
@@ -47,5 +53,20 @@ public class GuavaOptionalTest {
     builder.foo("bar");
     final GuavaOptionals foobar = builder.build();
     assertThat(foobar.foo(), is(Optional.of("bar")));
+  }
+
+  @Test
+  public void verifySettingNonNullableNullNPEs() {
+    expectedException.expect(NullPointerException.class);
+    expectedException.expectMessage("foo");
+    builder.foo((Optional<String>)null);
+  }
+
+  @Test
+  public void verifySettingNullableNull() {
+    builder.bar("hello");
+    builder.baz((Optional<String>) null);
+    final GuavaOptionals foobar = builder.build();
+    assertThat(foobar.baz(), is(nullValue()));
   }
 }
