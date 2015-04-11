8461539bd4023dc36ebb0804dc962b04786db609
Marc Bruggmann <bruggmann.marc@gmail.com>
2015-04-11 16:22:51 +0200

    Account for improved import handling in tests.

--- processor/src/test/resources/expected/JUTOptionalFieldsBuilder.java
+++ processor/src/test/resources/expected/JUTOptionalFieldsBuilder.java
@@ -1,28 +1,17 @@
 package foo;
 
 import io.norberg.automatter.AutoMatter;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import java.util.Optional;
 import javax.annotation.Generated;
 
 @Generated("io.norberg.automatter.processor.AutoMatterProcessor")
 public final class JUTOptionalFieldsBuilder {
 
-  private java.util.Optional<String> foo;
-  private java.util.Optional<String> bar;
+  private Optional<String> foo;
+  private Optional<String> bar;
 
   public JUTOptionalFieldsBuilder() {
-    this.foo = java.util.Optional.empty();
+    this.foo = Optional.empty();
   }
 
   private JUTOptionalFieldsBuilder(JUTOptionalFields v) {
@@ -35,32 +24,32 @@ public final class JUTOptionalFieldsBuilder {
     this.bar = v.bar;
   }
 
-  public java.util.Optional<String> foo() {
+  public Optional<String> foo() {
     return foo;
   }
 
   public JUTOptionalFieldsBuilder foo(String foo) {
-    return foo(java.util.Optional.ofNullable(foo));
+    return foo(Optional.ofNullable(foo));
   }
 
-  public JUTOptionalFieldsBuilder foo(java.util.Optional<? extends String> foo) {
+  public JUTOptionalFieldsBuilder foo(Optional<? extends String> foo) {
     if (foo == null) {
       throw new NullPointerException("foo");
     }
-    this.foo = (java.util.Optional<String>) foo;
+    this.foo = (Optional<String>) foo;
     return this;
   }
 
-  public java.util.Optional<String> bar() {
+  public Optional<String> bar() {
     return bar;
   }
 
   public JUTOptionalFieldsBuilder bar(String bar) {
-    return bar(java.util.Optional.ofNullable(bar));
+    return bar(Optional.ofNullable(bar));
   }
 
-  public JUTOptionalFieldsBuilder bar(java.util.Optional<? extends String> bar) {
-    this.bar = (java.util.Optional<String>) bar;
+  public JUTOptionalFieldsBuilder bar(Optional<? extends String> bar) {
+    this.bar = (Optional<String>) bar;
     return this;
   }
 
@@ -79,11 +68,11 @@ public final class JUTOptionalFieldsBuilder {
   private static final class Value
       implements JUTOptionalFields {
 
-    private final java.util.Optional<String> foo;
-    private final java.util.Optional<String> bar;
+    private final Optional<String> foo;
+    private final Optional<String> bar;
 
-    private Value(@AutoMatter.Field("foo") java.util.Optional<String> foo,
-                  @AutoMatter.Field("bar") java.util.Optional<String> bar) {
+    private Value(@AutoMatter.Field("foo") Optional<String> foo,
+                  @AutoMatter.Field("bar") Optional<String> bar) {
       if (foo == null) {
         throw new NullPointerException("foo");
       }
@@ -93,13 +82,13 @@ public final class JUTOptionalFieldsBuilder {
 
     @AutoMatter.Field
     @Override
-    public java.util.Optional<String> foo() {
+    public Optional<String> foo() {
       return foo;
     }
 
     @AutoMatter.Field
     @Override
-    public java.util.Optional<String> bar() {
+    public Optional<String> bar() {
       return bar;
     }
 
