386272e401b94c365ffdc6aaf8a555f7d8d38939
Daniel Norberg <dano@spotify.com>
2015-06-01 14:17:31 -0400

    docker integration testing

--- jackson-it.sh
+++ jackson-it.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 mvn install -DskipTests=true
-for v in $(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.core%22+AND+a:%22jackson-databind%22&core=gav&rows=100&wt=json' |
+JACKSON=$(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.core%22+AND+a:%22jackson-databind%22&core=gav&rows=100&wt=json' |
            jq -r '[.response.docs | .[].v |
                   {p: split("."), v:.} |
                   {major:.p[0] | tonumber, minor: .p[1] | tonumber, v:.v} |
@@ -8,7 +8,8 @@ for v in $(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterx
                   sort_by([.major, .minor, .v]) |
                   .[].v' |
            grep -v '2.4.0-rc')
-do
+
+for v in ${JACKSON}; do
   echo
   echo "Testing Jackson $v"
   echo "========================================================================"
