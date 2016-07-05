9707aa40b680e55943ea50b1a9a1bc0a9b4494b6
Olle Lundberg <geek@nerd.sh>
2016-07-05 18:48:35 +0200

    Add test for @SerializedName.

--- gson/src/test/java/io/norberg/automatter/gson/Foo.java
+++ gson/src/test/java/io/norberg/automatter/gson/Foo.java
@@ -1,9 +1,12 @@
 package io.norberg.automatter.gson;
 
+import com.google.gson.annotations.SerializedName;
+
 import io.norberg.automatter.AutoMatter;
 
 @AutoMatter
 public interface Foo {
   int a();
   String b();
+  @SerializedName("private") Boolean isPrivate();
 }
