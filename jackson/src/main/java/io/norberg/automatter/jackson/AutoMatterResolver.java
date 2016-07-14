ed273e904eb4451a311f59bde615038426db65e5
Daniel Norberg <daniel.norberg@gmail.com>
2016-07-14 11:37:55 +0900

    fix jackson 2.8.0 compatibility

--- jackson/src/main/java/io/norberg/automatter/jackson/AutoMatterResolver.java
+++ jackson/src/main/java/io/norberg/automatter/jackson/AutoMatterResolver.java
@@ -1,9 +1,11 @@
 package io.norberg.automatter.jackson;
 
 import com.fasterxml.jackson.databind.AbstractTypeResolver;
+import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.JavaType;
 
+import java.lang.reflect.Type;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -13,19 +15,27 @@ class AutoMatterResolver extends AbstractTypeResolver {
 
   private static final String VALUE_SUFFIX = "Builder$Value";
 
-  private final ConcurrentMap<JavaType, JavaType> types = new ConcurrentHashMap<>();
+  private final ConcurrentMap<Class<?>, JavaType> types = new ConcurrentHashMap<>();
 
-  @Override
-  public JavaType resolveAbstractType(final DeserializationConfig config, final JavaType type) {
-    final Class<?> rawClass = type.getRawClass();
+  public JavaType resolveAbstractType(DeserializationConfig config, JavaType type)
+  {
+    return resolveAbstractType0(config, type.getRawClass());
+  }
+
+  public JavaType resolveAbstractType(DeserializationConfig config, BeanDescription typeDesc)
+  {
+    return resolveAbstractType0(config, typeDesc.getBeanClass());
+  }
+
+  private JavaType resolveAbstractType0(final DeserializationConfig config, final Class<?> rawClass) {
     final AutoMatter annotation = rawClass.getAnnotation(AutoMatter.class);
     if (annotation == null) {
       // This was not an @AutoMatter type.
-      return super.resolveAbstractType(config, type);
+      return null;
     }
 
     // Return the cached type, if present.
-    final JavaType cached = types.get(type);
+    final JavaType cached = types.get(rawClass);
     if (cached != null) {
       return cached;
     }
@@ -43,7 +53,7 @@ class AutoMatterResolver extends AbstractTypeResolver {
     final JavaType materialized = config.getTypeFactory().constructType(cls);
 
     // Cache the materialized type before returning
-    final JavaType existing = types.putIfAbsent(type, materialized);
+    final JavaType existing = types.putIfAbsent(rawClass, materialized);
     return (existing != null) ? existing : materialized;
   }
 }
