6a8bcb17ca846c0896fd0b6428189ddddb2a3ef2
Daniel Norberg <dano@spotify.com>
2017-08-28 17:34:25 +0200

    store versions in files

--- jackson-it.sh
+++ jackson-it.sh
@@ -4,25 +4,25 @@ set -euxo pipefail
 
 mvn install -DskipTests=true
 
-JACKSON_DATABIND=$(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.core%22+AND+a:%22jackson-databind%22&core=gav&rows=100&wt=json' |
+curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.core%22+AND+a:%22jackson-databind%22&core=gav&rows=100&wt=json' |
            jq -r '[.response.docs | .[].v |
                   {p: split("."), v:.} |
                   {major:.p[0] | tonumber, minor: .p[1] | tonumber, v:.v} |
                   select(.major >= 2 and .minor >= 4)] |
                   sort_by([.major, .minor, .v]) |
                   .[].v' |
-           grep -v '2.4.0-rc')
+           grep -v '2.4.0-rc' > jackson-databind-versions.txt
 
-JACKSON_JODA=$(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.datatype%22+AND+a:%22jackson-datatype-joda%22&core=gav&rows=100&wt=json' |
+curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.datatype%22+AND+a:%22jackson-datatype-joda%22&core=gav&rows=100&wt=json' |
            jq -r '[.response.docs | .[].v |
                   {p: split("."), v:.} |
                   {major:.p[0] | tonumber, minor: .p[1] | tonumber, v:.v} |
                   select(.major >= 2 and .minor >= 4)] |
                   sort_by([.major, .minor, .v]) |
                   .[].v' |
-           grep -v '2.4.0-rc')
+           grep -v '2.4.0-rc' > jackson-joda-versions.txt
 
-JACKSON=$(comm -12 <(echo JACKSON_DATABIND) <(echo $JACKSON_JODA))
+JACKSON=$(comm -12 jackson-databind-versions.txt jackson-joda-versions.txt)
 
 for v in ${JACKSON}; do
   echo
