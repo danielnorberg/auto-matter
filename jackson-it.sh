5b9e67292289c83a16c72d5f19b1fde80a13d188
Daniel Norberg <dano@spotify.com>
2017-08-28 17:27:08 +0200

    fix jackson IT

--- jackson-it.sh
+++ jackson-it.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 mvn install -DskipTests=true
-JACKSON=$(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.core%22+AND+a:%22jackson-databind%22&core=gav&rows=100&wt=json' |
+
+JACKSON_DATABIND=$(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.core%22+AND+a:%22jackson-databind%22&core=gav&rows=100&wt=json' |
            jq -r '[.response.docs | .[].v |
                   {p: split("."), v:.} |
                   {major:.p[0] | tonumber, minor: .p[1] | tonumber, v:.v} |
@@ -8,6 +9,17 @@ JACKSON=$(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxm
                   sort_by([.major, .minor, .v]) |
                   .[].v' |
            grep -v '2.4.0-rc')
+
+JACKSON_JODA=$(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.datatype%22+AND+a:%22jackson-datatype-joda%22&core=gav&rows=100&wt=json' |
+           jq -r '[.response.docs | .[].v |
+                  {p: split("."), v:.} |
+                  {major:.p[0] | tonumber, minor: .p[1] | tonumber, v:.v} |
+                  select(.major >= 2 and .minor >= 4)] |
+                  sort_by([.major, .minor, .v]) |
+                  .[].v' |
+           grep -v '2.4.0-rc')
+
+JACKSON=$(comm -12 <(echo $JACKSON) <(echo $JACKSON_JODA))
 
 for v in ${JACKSON}; do
   echo
