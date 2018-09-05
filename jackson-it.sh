597d4c199112aaa788351a611896e102f0fcc3ef
Daniel Norberg <daniel.norberg@gmail.com>
2018-09-05 11:22:56 +0900

    retry jackson version lookup

--- jackson-it.sh
+++ jackson-it.sh
@@ -1,19 +1,35 @@
 #!/bin/bash -e
-JACKSON=$(curl -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.core%22+AND+a:%22jackson-databind%22&core=gav&rows=100&wt=json' |
-           jq -r '[.response.docs | .[].v |
-                  {p: split("."), v:.} |
-                  {major:.p[0] | tonumber, minor: .p[1] | tonumber, v:.v} |
-                  select(.major >= 2 and .minor >= 4)] |
-                  sort_by([.major, .minor, .v]) |
-                  .[].v' |
-           grep -v '2.4.0-rc')
 
-for v in ${JACKSON}; do
+function lookup_jackson_versions0() {
+  curl -v -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.core%22+AND+a:%22jackson-databind%22&core=gav&rows=100&wt=json' |
+  jq -r '[.response.docs | .[].v |
+         {p: split("."), v:.} |
+         {major:.p[0] | tonumber, minor: .p[1] | tonumber, v:.v} |
+         select(.major >= 2 and .minor >= 4)] |
+         sort_by([.major, .minor, .v]) |
+         .[].v' |
+  grep -v '2.4.0-rc'
+}
+
+function lookup_jackson_versions() {
+  logfile=$(mktemp)
+  for _ in {1..30}; do
+    if lookup_jackson_versions0 2>"$logfile"; then
+      return 0
+    fi
+    sleep 1
+  done
+  >&2 echo "Failed to get jackson versions: "
+  >&2 cat "$logfile"
+  exit 1
+}
+
+for v in $(lookup_jackson_versions); do
   echo
   echo "Testing Jackson $v"
   echo "========================================================================"
   set -x
-  mvn -nsu -B -Djackson.version=$v -pl jackson surefire:test
+  mvn -nsu -B -Djackson.version="$v" -pl jackson surefire:test
   set +x
 done
 
