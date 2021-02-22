05de0ef651baa6d50bf19f707005413ca56f0351
Daniel Norberg <dano@spotify.com>
2021-02-22 15:54:22 -0500

    fix bom IT

--- jackson-it.sh
+++ jackson-it.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
 
 function lookup_jackson_versions0() {
-  curl -v -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson.core%22+AND+a:%22jackson-databind%22&core=gav&rows=100&wt=json' |
+  curl -v -s 'http://search.maven.org/solrsearch/select?q=g:%22com.fasterxml.jackson%22+AND+a:%22jackson-bom%22&core=gav&rows=1000&wt=json' |
   jq -r '[.response.docs | .[].v |
          {p: split("."), v:.} |
          {major:.p[0] | tonumber, minor: .p[1] | tonumber, v:.v} |
          select(.major >= 2 and .minor >= 4)] |
          sort_by([.major, .minor, .v]) |
          .[].v' |
-  grep -v '2.4.0-rc'
+  grep -E -v '(rc|pr)' |
+  grep -E -v '\.\d{8}'
 }
 
 function lookup_jackson_versions() {
@@ -34,7 +35,7 @@ for v in ${jackson_versions}; do
   echo "Testing Jackson $v"
   echo "========================================================================"
   set -x
-  mvn -nsu -B -Djackson.version="$v" -pl jackson surefire:test
+  mvn -nsu -B -Djackson.bom.version="$v" -pl jackson surefire:test
   set +x
 done
 
